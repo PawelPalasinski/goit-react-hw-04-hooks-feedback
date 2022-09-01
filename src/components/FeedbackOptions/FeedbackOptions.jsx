3b83592dc4c280070509325fa53ede32529c6d83
PawelPalasinski <pawel.palasinski@gmail.com>
2022-09-01 21:26:52 +0200

    fn FeedbackOptions reworked

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 
-export const FeedbackOptionsV2 = () => {
+export const FeedbackOptions = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
